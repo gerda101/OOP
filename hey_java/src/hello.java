9e0123141d961e90706334bd89ca25c57f0a0a12
urban10 <urban10@lab104-18.iit.uni-miskolc.hu>
2018-04-16 16:49:20 +0200

    java

---
+++ hey_java/src/hello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author urban10
+ *
+ */
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!\n\nBye");
+	}
+
+}
